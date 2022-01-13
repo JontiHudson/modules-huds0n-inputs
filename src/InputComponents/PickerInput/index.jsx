81d7ce7df82c849360fb9b3f07dcd939a80c854c
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-13 09:07:18 +0000

    1.6.0-beta46

--- src/InputComponents/PickerInput/index.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PickerInput = void 0;
-const tslib_1 = require("tslib");
-const CustomInput_1 = require("../CustomInput");
-const InputComponent_1 = require("./InputComponent");
-const OutputComponent_1 = require("./OutputComponent");
-exports.PickerInput = (0, CustomInput_1.createCustomInput)(OutputComponent_1.OutputComponent, InputComponent_1.InputComponent);
-(0, tslib_1.__exportStar)(require("./helpers"), exports);
